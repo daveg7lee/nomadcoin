7a04f762a46a4a52b8b95fa742a0c482afb74ff8
Dave <daveg7lee@gmail.com>
2022-03-12 20:15:14 +0900

    Rendering Templates

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/daveg7lee/nomadcoin/block"
+	"github.com/daveg7lee/nomadcoin/blockchain"
 )
 
 const port string = ":4000"
 
+type homeData struct {
+	PageTitle string
+	Blocks    []*block.Block
+}
+
 func handleHome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello from home!")
+	html := template.Must(template.ParseFiles("./templates/home.html"))
+	data := homeData{PageTitle: "Home", Blocks: blockchain.GetBlockchain().GetAllBlocks()}
+	html.Execute(w, data)
 }
 
 func main() {
