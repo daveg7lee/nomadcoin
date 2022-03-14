121bb65238371bce3e09e85aae99b9fb2038be7e
Dave <daveg7lee@gmail.com>
2022-03-14 19:57:39 +0900

    Parsing Commands

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-	"github.com/daveg7lee/nomadcoin/explorer"
-	"github.com/daveg7lee/nomadcoin/rest"
+	"os"
+
+	"github.com/fatih/color"
 )
 
 func main() {
-	go rest.Start(4000)
-	explorer.Start(5000)
+	boldText := color.New(color.FgHiWhite, color.Bold)
+
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		boldText.Println("Start HTML Explorer")
+	case "rest":
+		boldText.Println("Start REST API")
+	default:
+		usage()
+	}
+}
+
+func usage() {
+	color.Yellow("Welcome to Nomad Coin\n\n")
+	color.Red("Please use the following commands:\n\n")
+	color.Cyan("explorer:     Start the HTML Explorer")
+	color.Cyan("rest:         Start the REST API (recommended)")
+	os.Exit(0)
 }
