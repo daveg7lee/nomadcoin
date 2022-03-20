d7d7fe3a84388a7ad5e928383afb0daf85d04646
Dave <daveg7lee@gmail.com>
2022-03-20 19:28:43 +0900

    Verifying Messages

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,28 +1,11 @@
 package wallet
 
-import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"encoding/hex"
-	"fmt"
-
-	"github.com/daveg7lee/nomadcoin/utils"
+const (
+	hashedMessage string = "253b27734439cc9ccda1d6efe98ab811d181a848a28b2291f791a96d738bc2a6"
+	privateKey    string = "30770201010420d2dd1247794c617dc567a208773d9f28b8bb1410c70c9ed1880e4e275129329da00a06082a8648ce3d030107a14403420004563ad758a3f04b28afdc3c350336516ec58e265d611213a5e28ee8e222fac34d368bf6f9001e2eaed5a2260aa9b06c7a998a22707e260da38747bc65fbe4a441"
+	signature     string = "34504d2f918ad6abced891abdf05b424c2c7ffd30722733458abdfaca101529bd493005d0eea5b256a49c54e1f36fb4d56cccee526dbae497345dd7dea76c831"
 )
 
 func Start() {
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	utils.HandleErr(err)
 
-	message := "I am the message"
-
-	hashedMessage := utils.Hash(message)
-
-	hashAsBytes, err := hex.DecodeString(hashedMessage)
-	utils.HandleErr(err)
-
-	r, s, err := ecdsa.Sign(rand.Reader, privateKey, hashAsBytes)
-	utils.HandleErr(err)
-
-	fmt.Printf("R:%d\nS:%d\n", r, s)
 }
