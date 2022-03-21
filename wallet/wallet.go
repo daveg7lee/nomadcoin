890a79c234531d0e6afb062751b9cabe667263c2
Dave <daveg7lee@gmail.com>
2022-03-21 20:14:59 +0900

    Verification Function

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -7,6 +7,7 @@ import (
 	"crypto/x509"
 	"encoding/hex"
 	"fmt"
+	"math/big"
 	"os"
 
 	"github.com/daveg7lee/nomadcoin/utils"
@@ -90,6 +91,39 @@ func Sign(payload string, w *wallet) string {
 	return fmt.Sprintf("%x", signature)
 }
 
-func Verify(signature, payload, publicKey string) bool {
+func Verify(signature, payload, address string) bool {
+	r, s, err := restoreBigInts(signature)
+	utils.HandleErr(err)
 
+	x, y, err := restoreBigInts(address)
+	utils.HandleErr(err)
+
+	publicKey := ecdsa.PublicKey{
+		Curve: elliptic.P256(),
+		X:     x,
+		Y:     y,
+	}
+
+	payloadAsBytes, err := hex.DecodeString(payload)
+	utils.HandleErr(err)
+
+	ok := ecdsa.Verify(&publicKey, payloadAsBytes, r, s)
+
+	return ok
+}
+
+func restoreBigInts(payload string) (*big.Int, *big.Int, error) {
+	bytes, err := hex.DecodeString(payload)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	firstHalfBytes := bytes[:len(bytes)/2]
+	secondHalfBytes := bytes[len(bytes)/2:]
+
+	bigA, bigB := big.Int{}, big.Int{}
+	bigA.SetBytes(firstHalfBytes)
+	bigB.SetBytes(secondHalfBytes)
+
+	return &bigA, &bigB, nil
 }
