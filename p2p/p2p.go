420c17d00fc786288175a09be5da52d212f64a74
Dave <daveg7lee@gmail.com>
2022-03-28 20:12:35 +0900

    Inbox

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,6 +3,7 @@ package p2p
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/daveg7lee/nomadcoin/utils"
 	"github.com/gorilla/websocket"
@@ -20,7 +21,14 @@ func Upgrade(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	utils.HandleErr(err)
 
-	initPeer(conn, ip, openPort)
+	peer := initPeer(conn, ip, openPort)
+
+	time.Sleep(2 * time.Second)
+	peer.inbox <- []byte("Hello too!")
+	peer.inbox <- []byte("Hello 2!")
+	peer.inbox <- []byte("Hello 3!")
+	peer.inbox <- []byte("Hello 4!")
+	peer.inbox <- []byte("Hello 5!")
 }
 
 func AddPeer(address, port, openPort string) {
@@ -28,6 +36,8 @@ func AddPeer(address, port, openPort string) {
 	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
 	utils.HandleErr(err)
 
-	initPeer(conn, address, port)
+	peer := initPeer(conn, address, port)
 
+	time.Sleep(time.Second)
+	peer.inbox <- []byte("Hello!")
 }
--- p2p/peer.go
+++ p2p/peer.go
@@ -9,25 +9,38 @@ import (
 var Peers map[string]*peer = make(map[string]*peer)
 
 type peer struct {
-	conn *websocket.Conn
+	conn  *websocket.Conn
+	inbox chan []byte
 }
 
 func (p *peer) read() {
 	for {
-		_, m, err := p.conn.ReadMessage()
+		_, message, err := p.conn.ReadMessage()
 		if err != nil {
 			break
 		}
-		fmt.Printf("%s", m)
+		fmt.Printf("%s", message)
+	}
+}
+
+func (p *peer) write() {
+	for {
+		message := <-p.inbox
+		p.conn.WriteMessage(websocket.TextMessage, message)
 	}
 }
 
 func initPeer(conn *websocket.Conn, address, port string) *peer {
 	p := &peer{
 		conn,
+		make(chan []byte),
 	}
 	key := fmt.Sprintf("%s:%s", address, port)
+
 	go p.read()
+	go p.write()
+
 	Peers[key] = p
+
 	return p
 }
